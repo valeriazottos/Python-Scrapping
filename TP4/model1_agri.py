8295976a194d49b83bb7426567a45d58df9770db
Valeriazy <vzottos@gmail.com>
2022-07-23 15:26:57 -0300

    Comentarios model1_agri
    Comente lo que hicimos y les puse su path respectivo. igual tengo muchas dudas de como poner los path correctamente. Tampoco sé el nombre de los autores de donde extrajimos el modelo para poner el path!

--- TP4/model1_agri.py
+++ TP4/model1_agri.py
@@ -4,6 +4,10 @@ Name : model1
 Group : 
 With QGIS : 32208
 """
+        
+#Una vez añadido el raster layer, vamos a processing toolbox para crear el primer modelo. 
+
+#Setup necesario para poder correr los comando fuera de qgis
 
 from qgis.core import QgsProcessing
 from qgis.core import QgsProcessingAlgorithm
@@ -13,6 +17,22 @@ from qgis.core import QgsProcessingParameterFeatureSink
 from qgis.core import QgsCoordinateReferenceSystem
 import processing
 
+#seteamos 
+
+mainpath = "/Volumes/GoogleDrive-112553083728584115268/My Drive/Herramientas computacionales/Clae 4/input"
+suitin = "{}/SUIT/suit/hdr.adf".format(mainpath)
+adm2in = "{}/gadm41_USA_shp/gadm41_USA_2.shp".format(mainpath)
+outpath = "{}/_output/counties_agrisuit.csv".format(mainpath)
+junkpath = "{}/_output/junk".format(mainpath)
+junkfile = "{}/_output/junk/agrisuit.tif".format(mainpath)
+if not os.path.exists(mainpath + "/_output"):
+    os.mkdir(mainpath + "/_output")
+if not os.path.exists(junkpath):
+    os.mkdir(junkpath)
+
+
+
+#Se crean los algoritmos
 
 class Model1(QgsProcessingAlgorithm):
 
@@ -27,8 +47,42 @@ class Model1(QgsProcessingAlgorithm):
         feedback = QgsProcessingMultiStepFeedback(4, model_feedback)
         results = {}
         outputs = {}
+        
+# definimos WGS 84 SR
+crs_wgs84 = QgsCoordinateReferenceSystem("epsg:4326")
 
-        # Drop field(s)
+#######################################
+######### Warp (reproject)#############
+
+#Reproyectamos el shp suit, lo cual es muy importante en mapas porque les da distintas formas y tamaños. 
+#Nombramos al nuevo raster como agrisuit.
+
+
+        alg_params = {
+            'DATA_TYPE': 0,  # Use Input Layer Data Type
+            'EXTRA': '',
+            'INPUT': 'suit_e05c7935_08a7_4538_aaea_f43e3f359bf5',
+            'MULTITHREADING': False,
+            'NODATA': None,
+            'OPTIONS': '',
+            'RESAMPLING': 0,  # Nearest Neighbour
+            'SOURCE_CRS': None,
+            'TARGET_CRS': QgsCoordinateReferenceSystem('EPSG:4326'),#esta es la coordenada agregada.
+            'TARGET_EXTENT': None,
+            'TARGET_EXTENT_CRS': None,
+            'TARGET_RESOLUTION': None,
+            'OUTPUT': parameters['Agrisuit']
+        }
+        outputs['WarpReproject'] = processing.run('gdal:warpreproject', alg_params, context=context, feedback=feedback, is_child_algorithm=True)
+        results['Agrisuit'] = outputs['WarpReproject']['OUTPUT']
+        return results
+    
+#######################################
+######### Drop field(s)################
+
+#Limpiamos el shapefile de los Estados de Estados Unidos junto a sus counties dropeando variables que no vamos a necesitar. 
+#No le ponemos no,bre a la nueva base, no es necesario.
+
         alg_params = {
             'COLUMN': ['GID_0','NAME_0','GID_1','GID_2','HASC_2','CC_2','TYPE_2','NL_NAME 2','VARNAME_2','NL_NAME_1','NL_NAME_2',' ENGTYPE_2'],
             'INPUT': '/Volumes/GoogleDrive-112553083728584115268/My Drive/Herramientas computacionales/Clae 4/input/gadm41_USA_shp/gadm41_USA_2.shp',
@@ -40,7 +94,14 @@ class Model1(QgsProcessingAlgorithm):
         if feedback.isCanceled():
             return {}
 
-        # Add autoincremental field
+    
+####################################################
+######### Add autoincremental field ################
+
+
+#Usamos autoincremental tool y vamos a enumerar a partir de la base con las variables dropeadas.
+#Creamos un layer que se llama 'counties'.
+
         alg_params = {
             'FIELD_NAME': 'cid',
             'GROUP_FIELDS': [''],
@@ -59,7 +120,13 @@ class Model1(QgsProcessingAlgorithm):
         if feedback.isCanceled():
             return {}
 
-        # Zonal statistics
+###########################################
+######### Zonal statistics ################
+
+#Para hacer estadísticas de raster usamos el algorimo 'zonal statistics'.
+# A partir del input layer counties y del raster layer agrisuit tomamos el promedio de las el promedio de las estadísticas del raster en los counties.
+# Nombramos a esta nueva layer como Zonal que es una tabla con estadísticas. 
+
         alg_params = {
             'COLUMN_PREFIX': '_',
             'INPUT': 'Incremented_3450c202_4909_4f3e_9750_85d8671a535f',
@@ -74,26 +141,22 @@ class Model1(QgsProcessingAlgorithm):
         feedback.setCurrentStep(3)
         if feedback.isCanceled():
             return {}
+        
+###################################################################
+# write to CSV
+###################################################################
+# Esto fue extraído de clean 
+print('outputting the data')
 
-        # Warp (reproject)
-        alg_params = {
-            'DATA_TYPE': 0,  # Use Input Layer Data Type
-            'EXTRA': '',
-            'INPUT': 'suit_e05c7935_08a7_4538_aaea_f43e3f359bf5',
-            'MULTITHREADING': False,
-            'NODATA': None,
-            'OPTIONS': '',
-            'RESAMPLING': 0,  # Nearest Neighbour
-            'SOURCE_CRS': None,
-            'TARGET_CRS': QgsCoordinateReferenceSystem('EPSG:4326'),
-            'TARGET_EXTENT': None,
-            'TARGET_EXTENT_CRS': None,
-            'TARGET_RESOLUTION': None,
-            'OUTPUT': parameters['Agrisuit']
-        }
-        outputs['WarpReproject'] = processing.run('gdal:warpreproject', alg_params, context=context, feedback=feedback, is_child_algorithm=True)
-        results['Agrisuit'] = outputs['WarpReproject']['OUTPUT']
-        return results
+with open(outpath, 'w') as output_file:
+    fieldnames = [field.name() for field in counties_fields_autoid.fields()]
+    line = ','.join(name for name in fieldnames) + '\n'
+    output_file.write(line)
+    for f in counties_fields_autoid.getFeatures():
+        line = ','.join(str(f[name]) for name in fieldnames) + '\n'
+        output_file.write(line)
+
+print('DONE!')
 
     def name(self):
         return 'model1'

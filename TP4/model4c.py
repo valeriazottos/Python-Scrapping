cc8f62c0b70b2dedc6410bc764f9b625e42ef15b
Leyre Sáenz Guillén <108157291+leyresaenz1@users.noreply.github.com>
2022-07-24 22:20:01 -0300

    Update model4c.py
    Comentarios modelo 4c

--- TP4/model4c.py
+++ TP4/model4c.py
@@ -1,4 +1,3 @@
-
 """
 Model exported as python.
 Name : model4c
@@ -29,7 +28,7 @@ class Model4c(QgsProcessingAlgorithm):
         results = {}
         outputs = {}
 
-        # Reproject layer
+        # Reproject layer: input layer: countries_drop_fields--> target CRS: World_Cylindrical_Equal_Area (ESRI 54034). La nombramos: countries_reprojected. 
         alg_params = {
             'INPUT': 'Remaining_fields_7aae2a58_5ae1_4799_9314_142b34c322dc',
             'OPERATION': '',
@@ -43,7 +42,7 @@ class Model4c(QgsProcessingAlgorithm):
         if feedback.isCanceled():
             return {}
 
-        # Drop field(s)
+        # Drop field(s): En este modelo, calculamos el área de los países. Primero usamos drop fields y abrimos countries.shp, eliminamos variables y denominamos al output como: "countries_drop_fields"  
         alg_params = {
             'COLUMN': ['featurecla','scalerank','LABELRANK','SOVEREIGNT','SOV_A3','ADM0_DIF','LEVEL','TYPE','TLC','ADM0_A3','GEOU_DIF','GEOUNIT','GU_A3','SU_DIF','SUBUNIT','SU_A3','BRK_DIFF','NAME','NAME_LONG','BRK_A3','BRK_NAME','BRK_GROUP','ABBREV','POSTAL','FORMAL_EN','FORMAL_FR','NAME_CIAWF','NOTE_ADM0','NOTE_BRK','NAME_SORT','NAME_ALT','MAPCOLOR7','MAPCOLOR8','MAPCOLOR9','MAPCOLOR13','POP_EST','POP_RANK','POP_YEAR','GDP_MD','GDP_YEAR','ECONOMY','INCOME_GRP','FIPS_10','ISO_A2','ISO_A2_EH','ISO_A3_EH','ISO_N3','ISO_N3_EH','UN_A3','WB_A2','WB_A3','WOE_ID','WOE_ID_EH','WOE_NOTE','ADM0_ISO','ADM0_DIFF','ADM0_TLC','ADM0_A3_US','ADM0_A3_FR','ADM0_A3_RU','ADM0_A3_ES','ADM0_A3_CN','ADM0_A3_TW','ADM0_A3_IN','ADM0_A3_NP','ADM0_A3_PK','ADM0_A3_DE','ADM0_A3_GB','ADM0_A3_BR','ADM0_A3_IL','ADM0_A3_PS','ADM0_A3_SA','ADM0_A3_EG','ADM0_A3_MA','ADM0_A3_PT','ADM0_A3_AR','ADM0_A3_JP','ADM0_A3_KO','ADM0_A3_VN','ADM0_A3_TR','ADM0_A3_ID','ADM0_A3_PL','ADM0_A3_GR','ADM0_A3_IT','ADM0_A3_NL','ADM0_A3_SE','ADM0_A3_BD','ADM0_A3_UA','ADM0_A3_UN','ADM0_A3_WB','CONTINENT','REGION_UN','SUBREGION','REGION_WB','NAME_LEN','LONG_LEN','ABBREV_LEN','TINY','HOMEPART','MIN_ZOOM','MIN_LABEL','MAX_LABEL','LABEL_X','LABEL_Y','NE_ID','WIKIDATAID','NAME_AR','NAME_BN','NAME_DE','NAME_EN','NAME_ES','NAME_FA','NAME_FR','NAME_EL','NAME_HE','NAME_HI','NAME_HU','NAME_ID','NAME_IT','NAME_JA','NAME_KO','NAME_NL','NAME_PL','NAME_PT','NAME_RU','NAME_SV','NAME_TR','NAME_UK','NAME_UR','NAME_VI','NAME_ZH','NAME_ZHT','FCLASS_ISO','TLC_DIFF','FCLASS_TLC','FCLASS_US','FCLASS_FR','FCLASS_RU','FCLASS_ES','FCLASS_CN','FCLASS_TW','FCLASS_IN','FCLASS_NP','FCLASS_PK','FCLASS_DE','FCLASS_GB','FCLASS_BR','FCLASS_IL','FCLASS_PS','FCLASS_SA','FCLASS_EG','FCLASS_MA','FCLASS_PT','FCLASS_AR','FCLASS_JP','FCLASS_KO','FCLASS_VN','FCLASS_TR','FCLASS_ID','FCLASS_PL','FCLASS_GR','FCLASS_IT','FCLASS_NL','FCLASS_SE','FCLASS_BD','FCLASS_UA'],
             'INPUT': '/Volumes/GoogleDrive-112553083728584115268/My Drive/Herramientas computacionales/Clae 4/input/ne_10m_admin_0_countries/ne_10m_admin_0_countries.shp',
@@ -56,7 +55,7 @@ class Model4c(QgsProcessingAlgorithm):
         if feedback.isCanceled():
             return {}
 
-        # Save vector features to file
+        # Save vector features to file: guardamos a través de save features to file--> vector features: areas_out--> saved features: areas.csv
         alg_params = {
             'DATASOURCE_OPTIONS': '',
             'INPUT': 'Calculated_54a3c539_5fa5_4f60_b7be_7c5bee007d11',
@@ -71,7 +70,7 @@ class Model4c(QgsProcessingAlgorithm):
         if feedback.isCanceled():
             return {}
 
-        # Fix geometries
+        # Fix geometries: input layer: countries_reprojected--> fixed geometries: countries_fixgeo.
         alg_params = {
             'INPUT': 'Reprojected_849e78dd_a283_40e9_984a_dec786c8d969',
             'OUTPUT': parameters['Countries_fixgeo']
@@ -83,7 +82,7 @@ class Model4c(QgsProcessingAlgorithm):
         if feedback.isCanceled():
             return {}
 
-        # Field calculator
+        # Field calculator: input layer: countries_fixedgeo, calcule km2area, que es una variable float, result field lenght: 10, result field precision: 10. Fórmula: arera($geometry)/1000000. Lo nombramos: areas_out. Vemos que esto calcula el área. 
         alg_params = {
             'FIELD_LENGTH': 10,
             'FIELD_NAME': 'km2area',

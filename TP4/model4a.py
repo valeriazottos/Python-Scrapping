03e46646485a266c607b3219527e0154f7c096ec
Leyre Sáenz Guillén <108157291+leyresaenz1@users.noreply.github.com>
2022-07-24 22:17:46 -0300

    Update model4a.py
    Comentarios modelo 4a

--- TP4/model4a.py
+++ TP4/model4a.py
@@ -5,13 +5,11 @@ Group :
 With QGIS : 32208
 """
 
-
 from qgis.core import QgsProcessing
 from qgis.core import QgsProcessingAlgorithm
 from qgis.core import QgsProcessingMultiStepFeedback
 from qgis.core import QgsProcessingParameterFeatureSink
 import processing
-
 
 class Model4a(QgsProcessingAlgorithm):
 
@@ -27,7 +25,7 @@ class Model4a(QgsProcessingAlgorithm):
         results = {}
         outputs = {}
 
-        # Fix geometries - countries
+        # Fix geometries - countries: usamos la base clean.shp, la corregimos a través de la herramienta fix geometries en QGIS y lo renombramos fixgeo_countries.
         alg_params = {
             'INPUT': '/Volumes/GoogleDrive-112553083728584115268/My Drive/Herramientas computacionales/Clae 4/input/ne_10m_admin_0_countries/ne_10m_admin_0_countries.shp',
             'OUTPUT': parameters['Fixgeo_countries']
@@ -39,7 +37,8 @@ class Model4a(QgsProcessingAlgorithm):
         if feedback.isCanceled():
             return {}
 
-        # Intersection
+        # Vector overlay--> Intersection: unimos ambos(fixgeo_wlds y fixgeo_countries). De la input layer (fixgeo_wlds) nos quedamos con la variable GID y de la overlay layer (fixgeo_countries) nos quedamos con la variable ADMIN. Nombramos a esta intersección como "intersection"
+        # Esto lo que hace es que en cada país nos cuenta la cantidad de áreas que intersecan ambas variables: contar por cada país cuantos idiomas aparecen
         alg_params = {
             'INPUT': 'Fixed_geometries_ebe6d4e3_d8f4_4710_99e1_b56449d9af28',
             'INPUT_FIELDS': ['GID'],
@@ -55,7 +54,7 @@ class Model4a(QgsProcessingAlgorithm):
         if feedback.isCanceled():
             return {}
 
-        # Fix geometries - wlds
+        # Fix geometries - wlds: usamos la base clean.shp, la corregimos a través de la herramienta fix geometries en QGIS y lo renombramos fixgeo_wlds.
         alg_params = {
             'INPUT': '/Volumes/GoogleDrive-112553083728584115268/My Drive/Herramientas computacionales/Clae 4/output/clean.shp',
             'OUTPUT': parameters['Fixgeo_wlds']
@@ -67,7 +66,7 @@ class Model4a(QgsProcessingAlgorithm):
         if feedback.isCanceled():
             return {}
 
-        # Statistics by categories
+        # Statistics by categories : usamos como output: "intersection" (capa creada previamente), y calculamos el campo ADMIN--> lo llamamos outputcsv. Lo que genera esto es una tabla que calcula cuantas veces estaba en la base de languages cada uno de los países (languages by country).
         alg_params = {
             'CATEGORIES_FIELD_NAME': ['ADMIN'],
             'INPUT': 'Intersection_ab0ae4b2_b9d9_4231_97d4_caed37a04dd5',
@@ -92,3 +91,4 @@ class Model4a(QgsProcessingAlgorithm):
 
     def createInstance(self):
         return Model4a()
+

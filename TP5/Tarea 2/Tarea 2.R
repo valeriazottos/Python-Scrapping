3bf4f149990272b5034650b95bf8ef038ce5ae8c
Valeriazy <vzottos@gmail.com>
2022-07-31 20:43:16 -0300

    Corregí mapa de ggplot versión 2
    creamos variable mean para que no hayan 2 labels.

--- TP5/Tarea 2/Tarea 2.R
+++ TP5/Tarea 2/Tarea 2.R
@@ -22,7 +22,7 @@ library("tmap")
 library("broom")
 
 #seteamos directorio 
-setwd("C:/Users/Merced/Desktop/videos 2 y 3 2022/videos 2 y 3")
+setwd("/Volumes/GoogleDrive-112553083728584115268/My Drive/Herramientas computacionales/Clase 5/videos 2 y 3")
 
 #cargamos shapefile de Londres
 lnd <- readOGR(dsn = "data/london_sport.shp")
@@ -60,14 +60,13 @@ lnd_f <- broom::tidy(lnd)
 
 # This step has lost the attribute information associated with the lnd object. We can add it back using the left_join function from the dplyr package (see ?left_join).
 lnd$id <- row.names(lnd) # allocate an id variable to the sp data
-head(lnd@data, n = 2) # final check before join (requires shared variable name)
 lnd_f <- left_join(lnd_f, lnd@data) # join the data
 
 # The new lnd_f object contains coordinates alongside the attribute information associated with each London Borough. It is now straightforward to produce a map with ggplot2. coord_equal() is the equivalent of asp = T in regular plots with R:
 
 ## ----"Mapa de Thefts in London"-------------------------------
 mapggplot <- ggplot(lnd_f, aes(long, lat, group = group, fill = CrimeCount)) +
-  geom_polygon() + 
+  geom_polygon(colour="darkgray") + coord_equal() +
   geom_path(colour="black", lwd=0.05) + 
   coord_equal() +
   labs(x = "", y = "",
@@ -75,17 +74,66 @@ mapggplot <- ggplot(lnd_f, aes(long, lat, group = group, fill = CrimeCount)) +
   ggtitle("London Thefts") + 
   geom_shadowtext (aes (label = name),
                    check_overlap = TRUE,
-                   color = "white",
-                   size = 4) + 
-  scale_fill_distiller(palette = "Oranges",
+                   size = 3,
+                   hjust = 0.5,                            
+                   vjust = 0.5,                           
+                   color = "black",                       
+                   bg.color="white") + 
+  scale_fill_distiller(palette = "Reds",
                        direction = +1, 
                        name = "Thefts by borough")
-  
+
 mapggplot
 
-#guardamos el mapa
+#we save the map
 
 ggsave("mapggplot.png")
+
+# What is wrong with this map is that the borough is duplicated due to an excess of data per borough. We ask classmates what they did with this problem and me decided to create one variable so we can have only one label. 
+#We need less data, so first we check if the variables are in numeric format:  
+lapply(lnd_f, class) #or
+#sapply(lnd_f, class)
+
+#In order to create new variables and not change the original file, we make a copy.
+copylnd <- lnd_f
+
+# We transform the variables in numeric expression 
+copylnd$piece <- as.numeric(as.factor(copylnd$piece))
+copylnd$group <- as.numeric(as.factor(copylnd$group))
+copylnd$Pop_2001 <- as.numeric(as.character(copylnd$Pop_2001))
+copylnd$id <- as.numeric(as.character(copylnd$id))
+copylnd$ons_label <- as.numeric(as.character(copylnd$ons_label))
+
+#Estimation of the mean of the variables in the df copylnd
+vars_copylnd_mean <- copylnd %>%
+  group_by(name)%>%
+  summarise_all("mean")
+
+#Use the same code as before but adding data=vars_copylnd_mean 
+mapggplot1 <- ggplot(lnd_f, aes(long, lat, group = group, fill = CrimeCount)) +
+  geom_polygon(colour="darkgray") + coord_equal() +
+  geom_path(colour="black", lwd=0.05) + 
+  coord_equal() +
+  labs(x = "", y = "",
+       fill = "% of thefts") +
+  ggtitle("London Thefts") + 
+  geom_shadowtext (aes (label = name),
+                   check_overlap = TRUE,
+                   data = vars_copylnd_mean, #this is what we add
+                   size = 3,
+                   hjust = 0.5,                            
+                   vjust = 0.5,                           
+                   color = "black",                       
+                   bg.color="white") + 
+  scale_fill_distiller(palette = "Reds",
+                       direction = +1, 
+                       name = "Thefts by borough")
+
+mapggplot1
+
+#we save the map
+
+ggsave("mapggplot1.png")
 
 ###### TMAP ######
 # tmap was created to overcome some of the limitations of base graphics and ggmap.
@@ -103,7 +151,7 @@ maptmap <- tm_shape(lnd) +
   
 maptmap
 
-#guardamos el mapa
+#save the map
 tmap_save(maptmap, "maptmap.png")
 
 
